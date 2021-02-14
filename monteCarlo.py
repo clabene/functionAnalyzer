89409228413a1409ab7f7daae1a0e71bcb4058e2
Claudio Benedetti <claudio.benedetti@mail.polimi.it>
2021-02-14 19:21:38 +0100

    fix Monte Carlo points sampling

--- monteCarlo.py
+++ monteCarlo.py
@@ -1,15 +1,18 @@
 import random as rand
+from functionParser import getFunction
 
-from function import Function as F
-
-f = F()
+expr = "3*x^2"
+f=getFunction(expr)
 
 xmin, xmax = 5,12
-ymin, ymax = 0,1_000
+ymin, ymax = 0,2_000
 V = (ymax-ymin)*(xmax-xmin)
 N = 1_000_000
 
-sample_points = [(rand.random()*(xmax-xmin), rand.random()*(ymax-ymin)) for _ in range(N)]
+sample_points = [(rand.random()*(xmax-xmin)+xmin, rand.random()*(ymax-ymin)+ymin) for _ in range(N)]
 
-ratio = sum(f(x_i)>=y_i for x_i,y_i in sample_points)/N
+ratio = sum(f.doOperation({'x':x_i})>=y_i for x_i,y_i in sample_points)/N
 print(ratio*V)
+
+fi = getFunction("x^3")
+print(fi.doOperation({'x':xmax})-fi.doOperation({'x':xmin}))
