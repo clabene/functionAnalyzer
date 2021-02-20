41d463f0ed065698e9c655529b84d6074952670a
Claudio Benedetti <claudio.benedetti@mail.polimi.it>
2021-02-20 23:52:13 +0100

    fix assert & remove useless code

--- monteCarlo.py
+++ monteCarlo.py
@@ -1,11 +1,13 @@
 import random as rand
 from functionParser import getFunction
 
+"""
+TODO estimate outRange
+"""
 
 def monteCarlo(fun, inRanges, outRange, N=None):
   vars = fun.getVariables()
-  assert len(inRanges) == len(vars)
-  D=len(vars) # input dimensionality (nr of variables)
+  assert len(inRanges) >= len(vars)
   ranges=inRanges+[outRange] # sampling space
   V = 1 # volume of sampling region
   for xmin,xmax in ranges:
@@ -17,12 +19,3 @@ def monteCarlo(fun, inRanges, outRange, N=None):
   ratio/=N # percentage of points drawn below the function
   ratio*=V # approximation of percentage of sampling region below the function
   return ratio
-
-if __name__=="__main__":
-  expr = "3*x^2"
-  f=getFunction(expr)
-  inRanges = [(0,10)]
-  outRange = (0,300)
-  N=1_000_000
-  vol=monteCarlo(f, inRanges=inRanges, outRange=outRange, N=N)
-  print(vol)
