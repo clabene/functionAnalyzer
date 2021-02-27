f609ff4fe2a3c64d711c8267d6cff16bd1b31530
Claudio Benedetti <claudio.benedetti@mail.polimi.it>
2021-02-27 22:30:08 +0100

    make optimize() recursive using IFunction

--- function.py
+++ function.py
@@ -1,19 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from math import log
 from utils import factorial
 
 class IFunction(ABC):
-  pass
+  def __eq__(self, other):
+    return str(self) == str(other)
+  @abstractmethod
+  def optimize(self):
+    pass
 
 class Variable(IFunction):
   def __init__(self, name):
     self.name = name
-  def __eq__(self, v2):
-    return self.name == v2.name
+  def __str__(self):
+    return self.name
+  def optimize(self):
+    return self
 
 class Constant(IFunction):
   def __init__(self, value):
     self.value = value
+  def __str__(self):
+    return str(self.value)
+  def optimize(self):
+    return self
 
 class BinaryFunction(IFunction):
   def __init__(self, x1, x2, op):
@@ -50,67 +60,67 @@ class Identity(BinaryFunction):
   def __init__(self, x1):
     super(Identity, self).__init__(x1, None, lambda x,y: x)
   def optimize(self):
-    return self.x1
+    return self.x1.optimize()
 
 class Addition(BinaryFunction):
   def __init__(self, x1, x2):
     super(Addition, self).__init__(x1, x2, lambda x,y: x+y)
   def optimize(self):
-    if self.x1==0:
-      return self.x2
-    if self.x2==0:
-      return self.x1
+    if self.x1==Constant(0):
+      return self.x2.optimize()
+    if self.x2==Constant(0):
+      return self.x1.optimize()
 
 class Subtraction(BinaryFunction):
   def __init__(self, x1, x2):
     super(Subtraction, self).__init__(x1, x2, lambda x,y: x-y)
   def optimize(self):
-    if self.x1==0:
-      return self.x2
-    if self.x2==0:
-      return self.x1
+    if self.x1==Constant(0):
+      return self.x2.optimize()
+    if self.x2==Constant(0):
+      return self.x1.optimize()
     if self.x2==self.x1:
-      return 0
+      return Constant(0)
 
 class Multiplication(BinaryFunction):
   def __init__(self, x1, x2):
     super(Multiplication, self).__init__(x1, x2, lambda x,y: x*y)
   def optimize(self):
-    if self.x1==0 or self.x2==0:
-      return 0
-    if self.x2==1:
-      return self.x1
-    if self.x1==1:
-      return self.x2
+    if self.x1==Constant(0) or self.x2==Constant(0):
+      return Constant(0)
+    if self.x2==Constant(1):
+      return self.x1.optimize()
+    if self.x1==Constant(1):
+      return self.x2.optimize()
 
 class Division(BinaryFunction):
   def __init__(self, x1, x2):
     super(Division, self).__init__(x1, x2, lambda x,y: x/y)
   def optimize(self):
-    if self.x2==1:
-      return self.x1
-    if self.x1==1:
-      return self.x2
+    if self.x2==Constant(1):
+      return self.x1.optimize()
+    if self.x1==Constant(1):
+      return self.x2.optimize()
     if self.x1==self.x2:
-      return 1
+      return Constant(1)
 
 class Power(BinaryFunction):
   def __init__(self, x1, x2):
     super(Power, self).__init__(x1, x2, lambda x,y: x**y)
   def optimize(self):
-    if self.x2==1:
-      return self.x1
-    if (self.x1==1 and self.x2>=0) or self.x2==0:
-      return 1
+    if self.x2==Constant(1):
+      return self.x1.optimize()
+    if (self.x1==Constant(1) and self.x2>=Constant(0)) or self.x2==Constant(0):
+      return Constant(1)
 
 class Root(BinaryFunction):
   def __init__(self, x1, x2):
     super(Root, self).__init__(x1, x2, lambda x,y: x**(1/y))
   def optimize(self):
-    if self.x2==1:
-      return self.x1
-    if self.x2==0:
-      return 1
+    if self.x2==Constant(1):
+      return self.x1.optimize()
+    if self.x2==Constant(0):
+      return Constant(1)
 
 
 class Logarithm(BinaryFunction):
@@ -118,13 +128,13 @@ class Logarithm(BinaryFunction):
     super(Logarithm, self).__init__(x1, x2, log)
   def optimize(self):
     if self.x1==self.x2:
-      return 1
+      return Constant(1)
   
 
 class Factorial(BinaryFunction):
   def __init__(self, x1):
     super(Factorial, self).__init__(x1, None, lambda x,y: factorial(x))
   def optimize(self):
-    if self.x1==0 or self.x1==1:
-      return 1
+    if self.x1==Constant(0) or self.x1==Constant(1):
+      return Constant(1)
   
